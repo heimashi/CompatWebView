a21d513c4864424de2a505c84bcb1dcf13bd7053
shiwang <shiwang@rong360.com>
2018-01-04 10:41:34 +0800

    init

---
+++ app/src/main/java/com/sw/bridge/WebViewActivity.java
@@ -0,0 +1,17 @@
+package com.sw.bridge;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class WebViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+}
